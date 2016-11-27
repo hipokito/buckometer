604ce8fb2b0dec523500e19a612087ea01066f6b
hipokito <yforgamey@gmail.com>
2016-11-27 16:10:49 +0200

    initail app

---
+++ src/main/java/it/ethiclab/web/app1/App1.java
@@ -0,0 +1,53 @@
+package it.ethiclab.web.app1;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("app1-theme")
+@Widgetset("it.ethiclab.web.app1.App1Widgetset")
+@Push
+public class App1 extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener( e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "App1Servlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = App1.class, productionMode = false)
+    public static class App1Servlet extends VaadinServlet {
+    }
+}
